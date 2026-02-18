9c7057e6fcc68daeea4fc040a46dd0ae06fb04bf
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-02-18 16:12:56 +0700

    chore: Update HeroSection component

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from "next/navigation";
-import { useRef } from "react";
+import { 
+    useRef
+} from "react";
 import Slideshow from "../slideshow";
 
 export default function HeroSection() {
@@ -17,16 +19,16 @@ export default function HeroSection() {
                 <div className="w-full h-full bg-green-500"></div>
                 <div className="w-full h-full bg-yellow-500"></div>
             </Slideshow>
-            <div className="z-2 md:max-w-120 sm:max-w-[50vw] max-w-[80vw] w-fit h-fit gap-y-8 flex flex-col items-center justify-center">
+            <div className="z-2 md:max-w-160 sm:max-w-[50vw] max-w-[80vw] w-fit h-fit gap-y-8 flex flex-col items-center justify-center">
                 <div className="w-fit h-fit flex flex-col gap-y-4">
                     <h1 className="text-white text-4xl text-center font-bold">
-                        Portal digital Imbudee
+                        Portal Digital Desa Imbudee
                     </h1>
                     <h2 className="text-white text-center text-lg">
                         Semua urusan administrasi menjadi lebih cepat dan mudah dengan layanan digital berbasis website.
                     </h2>
                 </div>
-                <div className="w-fit h-fit flex items-center justofy-center gap-4">
+                <div className="w-fit h-fit flex items-start justify-center gap-4">
                     <button 
                         onClick={() => {
                             const rect = heroRef.current?.getBoundingClientRect() ;
@@ -36,13 +38,13 @@ export default function HeroSection() {
                                 behavior: "smooth",
                             });
                         }}
-                        className="w-40 px-6 py-2 text-white bg-emerald-500 rounded-full hover:bg-emerald-600 transition duration-100"
+                        className="w-42 px-6 py-2 text-white bg-emerald-500 rounded-full hover:bg-emerald-600 transition duration-100"
                     >
                         Jelajahi
                     </button>
                     <button 
-                        onClick={() => router.push("/surat")}
-                        className="w-40 px-6 py-2 text-white bg-amber-500 rounded-full hover:bg-amber-600 transition duration-100"
+                        onClick={() => router.push("/surat/templat")}
+                        className="w-42 px-6 py-2 text-white bg-amber-500 rounded-full hover:bg-amber-600 transition duration-100"
                     >
                         Buat Surat
                     </button>
