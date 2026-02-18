097887c64870eee3fb101c4caed7d65d425c73a4
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-02-18 16:12:23 +0700

    chore: Update DocsSection component

--- components/home/docs-section.tsx
+++ components/home/docs-section.tsx
@@ -4,12 +4,13 @@ export default function DocsSection() {
     return (
         // TODO: Gunakan gambar yang valid
         <section className="flex flex-col items-center justify-center px-16 py-12 gap-8">
-            <div className="w-full h-fit flex flex-col items-center justify-center gap-2">
+            <div className="w-full h-fit max-w-240 flex flex-col items-center justify-center gap-2">
                 <h1 className="text-2xl font-bold sm:text-left text-center">
                     Dokumentasi Desa
                 </h1>
-                <h2 className="text-md">
+                <h2 className="text-md text-center">
                     Lihat dokumentasi dari kegiatan-kegiatan yang telah dilaksanakan di Desa Imbudee.
+                    Kegiatan-kagiatan yang dilaksanakan bertujuan untuk kesejahteraan dan kemajuan masyarakat Desa Imbudee
                 </h2>
             </div>
             <div className="w-full max-w-240 h-fit flex items-center justify-center">
