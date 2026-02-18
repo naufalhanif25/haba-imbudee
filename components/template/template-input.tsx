1a61088e5f36cf14e3ddd66b7f4026784bdc5b86
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-02-18 16:15:06 +0700

    feat: Add TemplateInput component

---
+++ components/template/template-input.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+
+type TemplateAttrInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+    title: string;
+    className?: string;
+    type: string;
+    required?: boolean;
+}
+
+export default function TemplateInput({
+    title,
+    className,
+    type,
+    required,
+    ...props
+}: TemplateAttrInputProps) {
+    return (
+        <div className={className}>
+            <div className="flex items-center justify-center w-fit h-fit gap-1">
+                <h1 className="text-sm font-medium text-nowrap">
+                    {title}
+                </h1>
+                {required && (
+                    <p className="text-sm text-red-500 font-medium">
+                        *
+                    </p>
+                )}
+            </div>
+            <input 
+                type={type} 
+                className="w-full py-1 border-b-2 outline-none border-gray-400 focus:border-emerald-500 text-md text-nowrap"
+                placeholder={title}
+                {...props}
+            />
+        </div>
+    )
+}
