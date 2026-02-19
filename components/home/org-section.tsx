f4b06028d5f397fdd1e9722ae63a6d55d111d1cd
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-02-19 07:51:16 +0700

    feat: Add organization structure section

---
+++ components/home/org-section.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function OrgSection() {
+    return (
+        <section className="flex flex-col items-center justify-center px-16 py-12 gap-8">
+            <div className="w-full h-fit max-w-240 flex flex-col items-center justify-center gap-2">
+                <h1 className="text-2xl font-bold sm:text-left text-center">
+                    Susunan Organisasi Pemerintahan
+                </h1>
+                <h2 className="text-md text-center">
+                    Berikut adalah Susunan Organisasi Pemerintahan Gampong Imbudee yang menggambarkan susunan kepemimpinan serta pembagian tugas dalam menjalankan roda pemerintahan desa.
+                </h2>
+            </div>
+            <div className="w-full max-w-240 h-fit flex items-center justify-center rounded-xl overflow-hidden">
+                <Image 
+                    src="/struktur_organisasi.png"
+                    alt="Struktur organisasi"
+                    width={1080}
+                    height={720}
+                    className="w-full h-full object-cover"
+                />
+            </div>
+        </section>
+    );
+}
