bfc1508e9d0015b3984074545a43c518fd5efa5b
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-02-19 07:49:58 +0700

    style: Change container padding

--- components/home/docs-section.tsx
+++ components/home/docs-section.tsx
@@ -3,14 +3,14 @@ import Slider from "../slider";
 
 export default function DocsSection() {
     return (
-        <section className="flex flex-col items-center justify-center px-16 py-12 gap-8">
+        <section className="flex flex-col items-center justify-center px-16 py-8 gap-8">
             <div className="w-full h-fit max-w-240 flex flex-col items-center justify-center gap-2">
                 <h1 className="text-2xl font-bold sm:text-left text-center">
                     Dokumentasi Desa
                 </h1>
                 <h2 className="text-md text-center">
                     Lihat dokumentasi dari kegiatan-kegiatan yang telah dilaksanakan di Desa Imbudee.
-                    Kegiatan-kagiatan yang dilaksanakan bertujuan untuk kesejahteraan dan kemajuan masyarakat Desa Imbudee
+                    Kegiatan-kagiatan yang dilaksanakan bertujuan untuk kesejahteraan dan kemajuan masyarakat Desa Imbudee.
                 </h2>
             </div>
             <div className="w-full max-w-240 h-fit flex items-center justify-center">
