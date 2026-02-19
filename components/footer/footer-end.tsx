219788fa720252e106ca3a4fc60bfd08b5cc3f11
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-02-19 07:52:03 +0700

    chore: Update contact

--- components/footer/footer-end.tsx
+++ components/footer/footer-end.tsx
@@ -10,21 +10,30 @@ export default function FooterEnd() {
         <div className="w-screen py-6 px-12 flex md:flex-row flex-col gap-4 items-center md:justify-between justify-center bg-emerald-500">
             {/* TODO: URL masing-masing media sosial */}
             <span className="flex h-fit w-fit gap-4">
-                <button className="w-fit h-fit">
+                <button 
+                    className="w-fit h-fit"
+                    onClick={() => window.open("https://www.instagram.com/desaimbudee?igsh=azJyMXNjdHkycWZ6", "_blank")}
+                >
                     <Instagram 
                         {...footerIconProps} 
                         color="white"
                         strokeWidth={2}
                     />
                 </button>
-                <button className="w-fit h-fit">
+                <button 
+                    className="w-fit h-fit"
+                    onClick={() => {}}
+                >
                     <Facebook 
                         {...footerIconProps} 
                         color="white"
                         strokeWidth={2}
                     />
                 </button>
-                <button className="w-fit h-fit">
+                <button 
+                    className="w-fit h-fit"
+                    onClick={() => {}}
+                >
                     <Youtube 
                         {...footerIconProps} 
                         color="white"
