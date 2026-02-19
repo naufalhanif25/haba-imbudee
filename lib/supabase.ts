1c27c5065317aeca1cf34d17c8400905a9666d05
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-02-20 06:48:14 +0700

    fix: Undefined supabase keys error

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,13 @@
-import { createClient } from "@supabase/supabase-js";
+import { 
+    createClient, 
+    SupabaseClient 
+} from "@supabase/supabase-js";
 
-export const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-);
+export const getSupabaseClient = (): SupabaseClient => {
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+    const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+
+    return createClient(supabaseUrl, supabaseKey);
+};
+
+export const supabase = getSupabaseClient();
