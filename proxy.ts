f337009956f80bfaf50a7b16e1b95e3b4eaf3b90
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-02-20 00:23:52 +0700

    feat: Add middleware fot /surat/buat

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 export function proxy(request: NextRequest) {
     const token = request.cookies.get("admin_token");
-    const { pathname } = request.nextUrl;
+    const { pathname, searchParams } = request.nextUrl;
 
     if (!token && pathname === "/admin/dashboard") {
         return NextResponse.redirect(new URL("/admin/masuk", request.url));
@@ -13,12 +13,20 @@ export function proxy(request: NextRequest) {
         return NextResponse.redirect(new URL("/admin/dashboard", request.url));
     }
 
+    if (pathname === "/surat/buat") {
+        const id = searchParams.get("id");
+        if (!id) {
+            return NextResponse.redirect(new URL("/surat/templat", request.url));
+        }
+    }
+
     return NextResponse.next();
 }
 
 export const config = {
     matcher: [
         "/admin/dashboard",
-        "/admin/masuk"
+        "/admin/masuk",
+        "/surat/buat"
     ],
 };
