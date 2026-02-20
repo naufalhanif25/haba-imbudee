88596d190a5626b311198bf3a6e291262f692d5f
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-02-20 15:32:16 +0700

    chore: Style cannot be undefined

--- app/admin/dashboard/(template)/template-props.ts
+++ app/admin/dashboard/(template)/template-props.ts
@@ -24,7 +24,7 @@ export type PlaceholderElement = {
     height: number;
     bounds?: string;
     className?: string;
-    style?: PlaceholderStyle;
+    style: PlaceholderStyle;
 };
 
 export type TamplatePlaceholder = {
